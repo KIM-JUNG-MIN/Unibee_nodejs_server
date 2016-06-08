655d8932cb744e310c3c9eac1bc2d1e02ec2e738
fg-unibee <iridescent.kjm17@gmail.com>
2016-06-08 15:55:16 +0900

    home/login/register.jade UI 디자인 완료 및 코드 연동

---
+++ app_server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var auth = require('./routes/auth');
+var app = express();
+var mysql = require('mysql');
+
+var options = {
+  host:'fgdbinstance.cmclvpcsh0vw.ap-northeast-1.rds.amazonaws.com',
+  port:'3306',
+  user:'ikbee',
+  password:'dkffkqb77',
+  database : 'FG_DB'
+};
+
+var connection = mysql.createConnection(options);
+
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/auth', auth(connection, options, app, router));
+app.set('views', './views'); //템플릿 엔진 위치
+app.set('view engine', 'jade'); //템플릿 엔진 지정
+
+app.get('/main', function(req, res){
+
+  if(req.user && req.user.displayName) {
+    res.render('login_success', {displayName:req.user.displayName});
+  } else {
+    res.render('home');
+  }
+});
+//홈 화면
+
+app.listen(9000, function(req, res){ //포트 리스닝
+  console.log("this is aws 9000 port server");
+})
---
+++ routes/auth.js
@@ -0,0 +1,128 @@
+module.exports = function(connection, options, app, router) {
+
+  var session = require('express-session');
+  var MySQLStore = require('express-mysql-session')(session);
+  var bodyParser = require('body-parser'); //post방식의 데이터에 접근할 수 있게 해주는 미들웨어
+  var bkfd2Password = require("pbkdf2-password");
+  var passport = require('passport')
+  var LocalStrategy = require('passport-local').Strategy;
+  var hasher = bkfd2Password();
+  var sessionStore = new MySQLStore({options}, connection);
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(session({
+    secret: 'sid',
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore
+  }))
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  //session 보다 뒤에 위치해야 한다!
+
+
+  router.get('/register', function(req, res) {
+    res.render('register_form');
+  });
+  //회원가입 form
+
+  router.get('/login', function(req, res) {
+    res.render('login_form');
+  });
+  //로그인 form
+
+  router.post('/register', function(req, res){
+
+    hasher({password:req.body.password}, function(err, pass, salt, hash) {
+      var user = {
+        authId: 'local:' + req.body.username,
+        username : req.body.username,
+        password : hash,
+        salt : salt,
+        displayName : req.body.displayName
+      };
+
+      var sql = 'INSERT INTO users SET ?';
+      connection.query(sql, user , function(err, result){
+        if (err) {
+          console.log(err);
+          res.status(500);
+        } else {
+          req.login(user, function(err){
+            req.session.save(function(){
+              res.redirect('/main');
+            });
+          });
+        }
+      });
+    });
+  });
+  //회원가입 form에서 받은 데이터로 사용자 추가
+
+  passport.serializeUser(function(user, done) {
+    console.log("serializeUser" , user);
+    done(null, user.authId);
+  });
+   //session 정보 처음 저장
+
+  passport.deserializeUser(function(id, done) {
+    console.log("deserializeUser" , id);
+    var sql = 'SELECT * FROM users WHERE authId =?';
+    connection.query(sql, [id] , function(err, results){
+      if (err) {
+        done('There is no user.');
+      }else {
+        done(null, results[0]);
+      }
+    });
+  });
+   //등록된 session 호출
+
+  passport.use(new LocalStrategy(
+    function(username, password, done){
+      var uname = username;
+      var pwd = password;
+
+
+      var sql = 'SELECT * FROM users WHERE authId =?';
+      connection.query(sql, ['local:'+uname] , function(err, results){
+        if (err) {
+          return done('There is no user.');
+        }
+        var user = results[0];
+        return hasher({password:pwd, salt:user.salt}, function(err, pass, salt, hash){
+          if(hash === user.password){
+            console.log('LocalStrategy', user);
+            done(null, user);
+          } else {
+            done(null, false);
+          }
+        });
+      });
+    }
+  ));
+
+  router.post('/login',
+    passport.authenticate('local',
+    { //successRedirect: '/welcome',
+      failureRedirect: '/login',
+      failureFlash: true }),
+      function(req, res){
+        req.session.save(function(){
+          res.redirect('/main');
+        });
+      } //미들웨어
+  );
+
+  router.get('/logout', function(req, res) {
+    req.logout();
+
+    req.session.save(function(){
+      res.redirect('/main');
+    });
+  });
+  //로그아웃
+
+  return router;
+}
